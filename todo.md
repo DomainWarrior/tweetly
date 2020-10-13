2e159e50c0c0acaa85fcd9a9c549fb88ca41e9cc
Domain Warrior <45982363+DomainWarrior@users.noreply.github.com>
2020-10-13 12:57:46 -0400

    initalizing again
    :) More files

---
+++ todo.md
@@ -0,0 +1,49 @@
+Short term todos
+
+Tweets
+ 
+    -> creating
+        ->Text
+        ->image -aws? media storage
+        
+   User Permissions
+   
+    -> delete
+    -> share
+    -> like
+    
+    
+Users
+
+    -> register
+    -> login
+    -> logout
+    
+    -> profile
+    
+        -> follow button
+        -> image
+        -> text
+        
+        
+    -> feed
+         -> personal feed only?
+         -> user and their following feed?
+    
+   Following / Followers
+   
+    -> Ignore
+    -> follow
+    -> Block
+  -----------  
+  
+  
+LONG TERM TODOS:
+
+    -> notifications
+
+    -> dms
+
+    -> explore/ finding hashtags / other users
+
+
---
+++ tweets/models.py
@@ -0,0 +1,23 @@
+import random
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
+
+class Tweet(models.Model):
+    # Maps to SQL data
+    # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)  # many users can many tweets
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
+        }
