9e83dc22704ad50147789908a151aebb2050cf62
Domain Warrior <45982363+DomainWarrior@users.noreply.github.com>
2020-10-15 17:15:35 -0400

    Tweetly-app-react-web
    Web version of react intergrated with django and rest api- this is the src/public repo for the web version

---
+++ tweetly-web/build/precache-manifest.90b56c340c1c80ef59f64eeb49e900d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b849f2bf5c5066b87bce83460399503e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "994dac5e1ebe3d808119",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3ac8321a3a2270f849a",
+    "url": "/static/js/2.e4b411e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4b411e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994dac5e1ebe3d808119",
+    "url": "/static/js/main.949fc5cf.chunk.js"
+  },
+  {
+    "revision": "85bd3a2269bd4eb75a04",
+    "url": "/static/js/runtime-main.450b860a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
